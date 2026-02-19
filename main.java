2e4c826e35bd673d0fec1cc214bd7b55ecc7db30
Harshini <harshinir785@gmail.com>
2026-02-19 14:30:40 +0530

    Added Experiment 3

---
+++ main.java
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FSAD Experiment 2</title>
+    <style>
+        body {
+            font-family: Arial;
+            text-align: center;
+            background-color: lightblue;
+        }
+        h1 {
+            color: darkblue;
+        }
+        button {
+            padding: 10px;
+            background-color: blue;
+            color: white;
+            border: none;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+
+    <h1>Welcome to My FSAD Project</h1>
+    <p>This is Experiment 2 - Frontend Development</p>
+
+    <button onclick="showMessage()">Click Me</button>
+
+    <script>
+        function showMessage() {
+            alert("Hello Harshini! Experiment 2 Working Successfully!");
+        }
+    </script>
+
+</body>
+</html>
